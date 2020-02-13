d60bb8226be46b548ab7417e7d5c40cdfc5d442b
Ahmad Abdolsaheb <ahmad.abdolsaheb@gmail.com>
2020-02-13 10:41:05 +0300

    Revert "feat: default to remote streams"
    This reverts commit df5a18252cbe82ebe3ed9b4f53332faa872d2523.

--- src/components/App.js
+++ src/components/App.js
@@ -275,7 +275,7 @@ export default class App extends React.Component {
           mounts: np.station.mounts,
           remotes: np.station.remotes
         });
-        this.setMountToConnection(np.station.mounts, np.station.remotes);
+        this.setMountToConnection(np.station.mounts);
       }
 
       if (this.state.listeners !== np.listeners.current) {
