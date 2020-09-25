12cfe5599679d2a406d06af9af72c7a7f94d48fa
max-voronov <70445727+max-voronov@users.noreply.github.com>
2020-09-25 11:32:58 +0300

    Refactor nav (#84)

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,85 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default class Nav extends React.Component {
-  state = {
-    navOpen: false
-  };
+export default function Nav() {
 
-  toggleSidenav() {
-    this.setState(prevState => ({
-      navOpen: !prevState.navOpen
-    }));
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidenav = () => {
+    setIsOpen(!isOpen);
   }
-  render() {
-    return (
-      <nav
-        className={
-          "site-nav nav-padding" + (this.state.navOpen ? " expand-nav" : "")
-        }
-        id="site-nav"
-      >
-        <div className="site-nav-left" />
-        <div className="site-nav-middle">
-          <a
-            className="site-nav-logo"
-            href="https://coderadio.freecodecamp.org/"
-          >
-            <img
-              alt="Code Radio"
-              src="https://cdn-media-1.freecodecamp.org/code-radio/FCC-logo.png"
-            />
-          </a>
-        </div>
-        <div className="site-nav-right main-nav">
-          <div className="main-nav-group">
-            <ul
-              className={
-                "nav" + (this.state.navOpen ? " show-main-nav-items" : "")
-              }
-              id="nav"
-              role="menu"
-            >
-              <li className="nav-news nav-current" role="menuitem">
-                <a
-                  href="https://www.freecodecamp.org/news/"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  News
-                </a>
-              </li>
-              <li className="nav-forum" role="menuitem">
-                <a
-                  href="https://www.freecodecamp.org/forum/"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  Forum
-                </a>
-              </li>
-              <li className="nav-learn" role="menuitem">
-                <a
-                  href="https://www.freecodecamp.org/learn/"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  Learn
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <button
-          className={
-            "site-nav-right toggle-button-nav" +
-            (this.state.navOpen ? " reverse-toggle-color" : "")
-          }
-          id="toggle-button-nav"
-          onClick={this.toggleSidenav.bind(this)}
+
+  const links = [
+    { href: "https://www.freecodecamp.org/news/", text: "News" },
+    { href: "https://www.freecodecamp.org/forum/", text: "Forum" },
+    { href: "https://www.freecodecamp.org/learn/", text: "Learn" }
+  ];
+
+  return (
+    <nav
+      className={
+        "site-nav" + (isOpen ? " expand-nav" : "")
+      }
+      id="site-nav"
+    >
+      <div className="site-nav-left" />
+      <div className="site-nav-middle">
+        <a
+          className="site-nav-logo"
+          href="https://coderadio.freecodecamp.org/"
         >
-          Menu
-        </button>
-      </nav>
-    );
-  }
+          <img
+            alt="Code Radio"
+            src="https://cdn-media-1.freecodecamp.org/code-radio/FCC-logo.png"
+          />
+        </a>
+      </div>
+      <div className="site-nav-right main-nav">
+        <div className="main-nav-group">
+          <ul
+            className={
+              "nav" + (isOpen ? " show-main-nav-items" : "")
+            }
+            id="nav"
+            role="menu"
+          >
+            {links.map((link, index) => (
+              <li key={index} role="menuitem">
+                <a
+                  href={link.href}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  {link.text}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <button
+        className={
+          "site-nav-right toggle-button-nav" +
+          (isOpen ? " reverse-toggle-color" : "")
+        }
+        id="toggle-button-nav"
+        onClick={toggleSidenav}
+      >
+        Menu
+      </button>
+    </nav>
+  );
 }
