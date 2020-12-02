88222d6a333f954f00c8547465f657ee13515070
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2020-12-02 07:38:14 +0100

    feat(sentry): default Sentry setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from "./components/App";
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new Integrations.BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
