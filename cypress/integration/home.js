22c7ac99ef70febf30b2eb1f7f85c26587085922
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2021-01-27 10:59:19 +0100

    feat: add Cypress testing

---
+++ cypress/integration/home.js
@@ -0,0 +1,7 @@
+/* global cy */
+describe('Landing page', () => {
+  it('Should render', () => {
+    cy.visit('http://localhost:3000');
+    cy.title().should('eq', 'freeCodeCamp.org Code Radio');
+  });
+});
