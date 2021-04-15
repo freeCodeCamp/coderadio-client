7b4c90b9e317a0d5fdc061f0e236f3fb9b27ac8a
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2021-04-15 18:01:14 +0700

    fix: use onTouchStart event in PlayPauseButton

--- src/components/PlayPauseButton.js
+++ src/components/PlayPauseButton.js
@@ -12,7 +12,7 @@ class PlayPauseButton extends React.Component {
 
   handleOnClick = () => isBrowser && this.props.togglePlay();
 
-  handleOnTouchEnd = () => !isBrowser && this.props.togglePlay();
+  handleOnTouchStart = () => !isBrowser && this.props.togglePlay();
 
   handleKeyDown = e => {
     // Toggle play when user presses Enter
@@ -38,7 +38,7 @@ class PlayPauseButton extends React.Component {
         id='playContainer'
         onClick={this.handleOnClick}
         onKeyDown={this.handleKeyDown}
-        onTouchEnd={this.handleOnTouchEnd}
+        onTouchStart={this.handleOnTouchStart}
       >
         {this.props.playing ? <Pause /> : <Play />}
       </button>
