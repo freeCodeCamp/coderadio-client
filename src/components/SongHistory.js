33c7a1456c5fde70933a6dcf350f99c1367d3fcb
Bruce B <bbsmooth@gmail.com>
2022-01-03 15:05:06 -0800

    fix: song history accessibility

--- src/components/SongHistory.js
+++ src/components/SongHistory.js
@@ -22,35 +22,52 @@ class SongHistory extends Component {
 
   render() {
     const { songHistory, fastConnection } = this.props;
-    const songs = songHistory.map(song => song.song).reverse();
+    // Don't reverse song list, we want most recent song first.
+    const songs = songHistory.map(song => song.song);
     return (
-      <button
-        aria-label='Recent Song History'
-        className='recent-song-history'
-        id='recent-song-history'
-        onClick={this.toggleDisplay}
-      >
-        {this.state.displayList && (
-          <div className='recent-song-list'>
-            {songs.map(song => (
-              <div className='recent-song-info' key={song.id}>
-                <img
-                  alt=''
-                  role='presentation'
-                  src={fastConnection ? song.art : DEFAULT_ART}
-                />
-                <div className='recent-song-meta'>
-                  <p>{song.title}</p>
-                  <p> {song.artist}</p>
-                  <p> {song.album}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-        )}
-
-        <FontAwesomeIcon className='recently-played-icon' icon={faHistory} />
-      </button>
+      <>
+        <button
+          aria-controls='song-history'
+          aria-expanded={this.state.displayList}
+          aria-label='Recent Song History'
+          className='recent-song-history'
+          id='recent-song-history'
+          onClick={this.toggleDisplay}
+        >
+          <FontAwesomeIcon
+            aria-hidden='true'
+            className='recently-played-icon'
+            icon={faHistory}
+          />
+        </button>
+        <ol
+          aria-hidden={!this.state.displayList}
+          className='recent-song-list'
+          id='song-history'
+        >
+          {songs.map((song, index) => (
+            <li className='recent-song-info' key={song.id}>
+              <img
+                alt=''
+                role='presentation'
+                src={fastConnection ? song.art : DEFAULT_ART}
+              />
+              <p className='recent-song-meta'>
+                <span>
+                  <span className='sr-only'>Song {index + 1}:</span>
+                  {song.title}
+                </span>
+                <span>
+                  <span className='sr-only'>, Artist:</span> {song.artist}
+                </span>
+                <span>
+                  <span className='sr-only'>, Album:</span> {song.album}
+                </span>
+              </p>
+            </li>
+          ))}
+        </ol>
+      </>
     );
   }
 }
