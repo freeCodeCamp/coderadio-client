6e670f03e25fcea2643fafa2968ac5a8f74ffa67
Bruce B <bbsmooth@gmail.com>
2022-01-10 16:50:52 -0800

    fix: nav menu accessibility

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -17,14 +17,27 @@ export default function Nav() {
     <nav className={'site-nav' + (isOpen ? ' expand-nav' : '')} id='site-nav'>
       <div className='site-nav-left' />
       <div className='site-nav-middle'>
-        <a className='site-nav-logo' href='https://coderadio.freecodecamp.org/'>
+        <a className='site-nav-logo' href='https://www.freecodecamp.org/'>
           <img
-            alt='Code Radio'
+            alt=''
             src='https://cdn.freecodecamp.org/platform/universal/fcc_primary.svg'
           />
+          <span className='sr-only'>freeCodeCamp.org home</span>
         </a>
       </div>
       <div className='site-nav-right main-nav'>
+        <button
+          aria-controls='nav'
+          aria-expanded={isOpen}
+          className={
+            'site-nav-right toggle-button-nav' +
+            (isOpen ? ' reverse-toggle-color' : '')
+          }
+          id='toggle-button-nav'
+          onClick={toggleSidenav}
+        >
+          Menu
+        </button>
         <div className='main-nav-group'>
           <ul
             className={'nav' + (isOpen ? ' show-main-nav-items' : '')}
@@ -33,23 +46,16 @@ export default function Nav() {
             {links.map((link, index) => (
               <li key={index}>
                 <a href={link.href} rel='noopener noreferrer' target='_blank'>
-                  {link.text}
+                  <span>
+                    {link.text}{' '}
+                    <span className='sr-only'>opens in new window</span>
+                  </span>
                 </a>
               </li>
             ))}
           </ul>
         </div>
       </div>
-      <button
-        className={
-          'site-nav-right toggle-button-nav' +
-          (isOpen ? ' reverse-toggle-color' : '')
-        }
-        id='toggle-button-nav'
-        onClick={toggleSidenav}
-      >
-        Menu
-      </button>
     </nav>
   );
 }
