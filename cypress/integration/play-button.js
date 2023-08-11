d88f5d7a4d5c4609e6696881bb75278ea7732ace
ahmad abdolsaheb <ahmad.abdolsaheb@gmail.com>
2023-08-11 10:34:03 +0300

    fix: cypress play

--- cypress/integration/play-button.js
+++ cypress/integration/play-button.js
@@ -1,5 +1,3 @@
-/* global cy expect*/
-
 describe('Stop and play the music', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000');
@@ -8,11 +6,9 @@ describe('Stop and play the music', () => {
   it('Click play button', () => {
     cy.get('audio')
       .invoke('attr', 'src')
-      .should('contain', 'freecodecamp.org/radio')
+      .should('contain', '.mp3')
       .then(() => {
-        cy.get('#toggle-play-pause')
-          .should('be.visible')
-          .click();
+        cy.get('#toggle-play-pause').should('be.visible').click();
         cy.get('audio').should(audioElements => {
           const audioIsPaused = audioElements[0].paused;
           expect(audioIsPaused).to.eq(false);
