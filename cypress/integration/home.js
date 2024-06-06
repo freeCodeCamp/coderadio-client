e30b357f3c46bd41c8c710329fa840bb4e7fb3bc
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-06-06 23:21:54 +0700

    fix: change default port to 3001

--- cypress/integration/home.js
+++ cypress/integration/home.js
@@ -1,7 +1,7 @@
 /* global cy */
 describe('Landing page', () => {
   it('Should render', () => {
-    cy.visit('http://localhost:3000');
+    cy.visit('http://localhost:3001');
     cy.title().should('eq', 'freeCodeCamp.org Code Radio');
   });
 });
