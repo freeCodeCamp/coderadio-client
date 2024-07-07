2aba3a8757672f9c0647c128519a36c9fabd76d2
Ahmad Abdolsaheb <ahmad.abdolsaheb@gmail.com>
2024-07-07 19:27:14 +0300

    fix: adjust subscription parameters. (#305)
    * fix: test new installation
    
    * Update src/components/App.js

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ import { buildEventSource } from '../utils/buildEventSource';
 import '../css/App.css';
 
 const sseUri =
-  'https://coderadio-admin-v2.freecodecamp.org/api/live/nowplaying/sse?cf_connect=%7B%22subs%22%3A%7B%22station%3Acoderadio%22%3A%7B%7D%2C%22global%3Atime%22%3A%7B%7D%7D%7D';
+  'https://coderadio-admin-v2.freecodecamp.org/api/live/nowplaying/sse?cf_connect=%7B%22subs%22%3A%7B%22station%3Acoderadio%22%3A%7B%22recover%22%3Atrue%7D%7D%7D';
 const jsonUri = `https://coderadio-admin-v2.freecodecamp.org/api/nowplaying_static/coderadio.json`;
 
 let sse = buildEventSource(sseUri);
